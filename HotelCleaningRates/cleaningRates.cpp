76990cb21aebe9386babe2076692a70c3ccd1c9e
Anthony Vuong <avuong04@calpoly.edu>
2020-10-04 15:59:10 -0700

    Updated hotel cleaning rates
    Big rooms AND small rooms included

--- HotelCleaningRates/cleaningRates.cpp
+++ HotelCleaningRates/cleaningRates.cpp
@@ -5,18 +5,29 @@ using namespace std;
 int main()
 {
     
-    int rooms {0}; 
-    const double room_price {32.5}; // const - will always be this value
+    int big_rooms {0}, small_rooms {0}; 
+    const double big_room_price {35.0}; // const - will always be this value
+    const double small_room_price {25.0}; 
     const double tax_rate {0.06};
     const int quote_expiration {30};
     
-    cout << "Enter number of rooms: ";
-    cin >> rooms;
+    cout << "Enter number of big rooms: ";
+    cin >> big_rooms;
     
-    cout << "You want " << rooms << " rooms." << endl;
-    cout << "Price per room is " << room_price << "." << endl;
+    cout << "Enter number of small rooms: ";
+    cin >> small_rooms;
+    
+    cout << "You want " << big_rooms << " big rooms." << endl;
+    cout << "And you want " << small_rooms << " small rooms." << endl;
+
+    cout << "Price per big room is " << big_room_price << "." << endl;
+    cout << "Price per small room is " << small_room_price << "." << endl;
     cout << "The tax rate is " << tax_rate << "." << endl;
-    cout << "For your quote we will charge " << (room_price * rooms) + (room_price * rooms * tax_rate) << "." << endl;
+    
+    double big_room_total = (big_room_price * big_rooms);
+    double small_room_total = (small_room_price * small_rooms);
+    double quote_total = (big_room_total + small_room_total) + ((big_room_total + small_room_total)* tax_rate);
+    cout << "For your quote we will charge " << quote_total << "." << endl;
  	cout << "And you have " << quote_expiration << " before this quote expires." << endl;
     return 0;
 }
